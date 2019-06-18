b6492e110612ce184d3ca82401de20c0cb8e5fbb
kaska <kaska.widlak@gmail.com>
2019-06-18 07:44:10 +0200

    add-migration problem

--- BillsApp/Data/ApplicationDbContext.cs
+++ BillsApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BillsAppDatabase;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,16 @@ namespace BillsApp.Data
             : base(options)
         {
         }
+
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<PaymentType> PaymentTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransactionCategory> TransactionCategories { get; set; }
+        public DbSet<TransactionElement> TransactionElements { get; set; }
+        public new DbSet<User> Users { get; set; }
+
     }
 }
